208de7d11f6650165b0f6f0eafcde620eade8cf6
s.k.noe@hinoshiba.com <s.k.noe@hinoshiba.com>
2018-10-19 21:50:02 +0900

    update

---
+++ editor/editor.go
@@ -0,0 +1,79 @@
+package editor
+
+import (
+	"os"
+	"os/exec"
+	"io"
+	"io/ioutil"
+)
+
+func Call(editor string, target []byte) ([]byte, error) {
+	return callEditor(editor, target)
+}
+
+func callEditor(editor string, target []byte) ([]byte, error) {
+	tdir, err := ioutil.TempDir("/var/tmp/", "editor-go-")
+	if err != nil {
+		return nil, err
+	}
+	defer os.RemoveAll(tdir)
+	tfile := tdir + "/tmpfile"
+
+	if err := writeFile(tfile, &target); err != nil {
+		return nil, err
+	}
+	if err := execEditor(tfile, editor); err != nil {
+		return nil, err
+	}
+
+	var ret []byte
+	if err := readFile(tfile, &ret) ;err != nil {
+		return nil, err
+	}
+	return ret, nil
+}
+
+func execEditor(path string, editor string) error {
+
+	cmd := exec.Command(editor, path)
+        cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func writeFile(path string, tag *[]byte) error {
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	file.Write(*tag)
+	return nil
+}
+
+func readFile(path string, ret *[]byte) error {
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	buf := make([]byte, 200)
+	for {
+		n, err := file.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+		*ret = append(*ret, buf[:n]...)
+	}
+	return nil
+}
